66be321b4f95b49f30d3f44f9a8db5b8c9939016
AdamMc331 <amcneilly331@gmail.com>
2017-05-18 00:34:26 -0400

    Converted MaterialPalettes to Kotlin for #15.

--- lib/src/androidTest/java/com/androidessence/materialdesignspecs/TestMaterialPalettes.java
+++ lib/src/androidTest/java/com/androidessence/materialdesignspecs/TestMaterialPalettes.java
@@ -23,9 +23,9 @@ public class TestMaterialPalettes extends AndroidTestCase{
         // color level. Verify by making sure the size of the return list is the size of our color
         // levels.
         try {
-            for(String colorName : MaterialPalettes.COLORS_WITH_ACCENT_NAMES) {
-                List<Integer> colors = MaterialPalettes.getColorsByName(colorName);
-                assertEquals(MaterialPalettes.COLOR_LEVELS.length, colors.size());
+            for(String colorName : MaterialPalettes.INSTANCE.getCOLORS_WITH_ACCENT_NAMES()) {
+                List<Integer> colors = MaterialPalettes.INSTANCE.getColorsByName(colorName);
+                assertEquals(MaterialPalettes.INSTANCE.getCOLOR_LEVELS().length, colors.size());
             }
         } catch(IllegalAccessException iae) {
             fail();
@@ -35,9 +35,9 @@ public class TestMaterialPalettes extends AndroidTestCase{
         // non-accent color levels. Verify by making sure the size of the return list is the size
         // of our color levels.
         try {
-            for(String colorName : MaterialPalettes.COLORS_WITHOUT_ACCENT_NAMES) {
-                List<Integer> colors = MaterialPalettes.getColorsByName(colorName);
-                assertEquals(MaterialPalettes.NON_ACCENT_COLOR_LEVELS.length, colors.size());
+            for(String colorName : MaterialPalettes.INSTANCE.getCOLORS_WITHOUT_ACCENT_NAMES()) {
+                List<Integer> colors = MaterialPalettes.INSTANCE.getColorsByName(colorName);
+                assertEquals(MaterialPalettes.INSTANCE.getNON_ACCENT_COLOR_LEVELS().length, colors.size());
             }
         } catch(IllegalAccessException iae) {
             fail();
@@ -45,7 +45,7 @@ public class TestMaterialPalettes extends AndroidTestCase{
 
         // Test that we throw an IllegalArgumentException if an invalid string is passed
         try {
-            List<Integer> colors = MaterialPalettes.getColorsByName("");
+            List<Integer> colors = MaterialPalettes.INSTANCE.getColorsByName("");
             // If we made it here, fail
             fail();
         } catch(IllegalArgumentException iae) {
@@ -56,7 +56,7 @@ public class TestMaterialPalettes extends AndroidTestCase{
 
         // Test that we throw an IllegalArgumentException if null is passed
         try {
-            List<Integer> colors = MaterialPalettes.getColorsByName(null);
+            List<Integer> colors = MaterialPalettes.INSTANCE.getColorsByName(null);
             // If we made it here, fail
             fail();
         } catch(IllegalArgumentException iae) {
@@ -75,8 +75,8 @@ public class TestMaterialPalettes extends AndroidTestCase{
         // If we get a list of colors for all colors with accents, our results should be
         // The number of color levels * number of colors
         try {
-            List<Integer> allColorsWithAccents = MaterialPalettes.getColorsByNames(MaterialPalettes.COLORS_WITH_ACCENT_NAMES);
-            int expectedCount = MaterialPalettes.COLOR_LEVELS.length * MaterialPalettes.COLORS_WITH_ACCENT_NAMES.length;
+            List<Integer> allColorsWithAccents = MaterialPalettes.INSTANCE.getColorsByNames(MaterialPalettes.INSTANCE.getCOLORS_WITH_ACCENT_NAMES());
+            int expectedCount = MaterialPalettes.INSTANCE.getCOLOR_LEVELS().length * MaterialPalettes.INSTANCE.getCOLORS_WITH_ACCENT_NAMES().length;
             assertEquals(expectedCount, allColorsWithAccents.size());
         } catch(IllegalAccessException iae) {
             fail();
@@ -85,8 +85,8 @@ public class TestMaterialPalettes extends AndroidTestCase{
         // If we get a list of colors for all colors without accents, our results should be
         // The number of non accent color levels * number of colors
         try {
-            List<Integer> allColorsWithoutAccents = MaterialPalettes.getColorsByNames(MaterialPalettes.COLORS_WITHOUT_ACCENT_NAMES, false);
-            int expectedCount = MaterialPalettes.NON_ACCENT_COLOR_LEVELS.length * MaterialPalettes.COLORS_WITHOUT_ACCENT_NAMES.length;
+            List<Integer> allColorsWithoutAccents = MaterialPalettes.INSTANCE.getColorsByNames(MaterialPalettes.INSTANCE.getCOLORS_WITHOUT_ACCENT_NAMES(), false);
+            int expectedCount = MaterialPalettes.INSTANCE.getNON_ACCENT_COLOR_LEVELS().length * MaterialPalettes.INSTANCE.getCOLORS_WITHOUT_ACCENT_NAMES().length;
             assertEquals(expectedCount, allColorsWithoutAccents.size());
         } catch(IllegalAccessException iae) {
             fail();
@@ -94,7 +94,7 @@ public class TestMaterialPalettes extends AndroidTestCase{
 
         // Test that we throw an IllegalArgumentException if an invalid string array is passed
         try {
-            List<Integer> colors = MaterialPalettes.getColorsByNames(new String[] { "", "" });
+            List<Integer> colors = MaterialPalettes.INSTANCE.getColorsByNames(new String[] { "", "" });
             // If we made it here, fail
             fail();
         } catch(IllegalArgumentException iae) {
@@ -105,7 +105,7 @@ public class TestMaterialPalettes extends AndroidTestCase{
 
         // Test that we throw an IllegalArgumentException if null is passed
         try {
-            List<Integer> colors = MaterialPalettes.getColorsByNames(null);
+            List<Integer> colors = MaterialPalettes.INSTANCE.getColorsByNames(null);
             // If we made it here, fail
             fail();
         } catch(IllegalArgumentException iae) {
@@ -125,9 +125,9 @@ public class TestMaterialPalettes extends AndroidTestCase{
         // For all color names, if we get the color list without accents we should return the size
         // of non_accent color levels
         try {
-            for(String colorName : MaterialPalettes.ALL_COLOR_NAMES) {
-                List<Integer> colors = MaterialPalettes.getColorsByName(colorName, false);
-                assertEquals(MaterialPalettes.NON_ACCENT_COLOR_LEVELS.length, colors.size());
+            for(String colorName : MaterialPalettes.INSTANCE.getALL_COLOR_NAMES()) {
+                List<Integer> colors = MaterialPalettes.INSTANCE.getColorsByName(colorName, false);
+                assertEquals(MaterialPalettes.INSTANCE.getNON_ACCENT_COLOR_LEVELS().length, colors.size());
             }
         } catch(IllegalAccessException iae) {
             fail();
@@ -135,7 +135,7 @@ public class TestMaterialPalettes extends AndroidTestCase{
 
         // Test that we throw an IllegalArgumentException if an invalid string is passed
         try {
-            List<Integer> colors = MaterialPalettes.getColorsByName("", false);
+            List<Integer> colors = MaterialPalettes.INSTANCE.getColorsByName("", false);
             // If we made it here, fail
             fail();
         } catch(IllegalArgumentException iae) {
@@ -146,7 +146,7 @@ public class TestMaterialPalettes extends AndroidTestCase{
 
         // Test that we throw an IllegalArgumentException if null is passed
         try {
-            List<Integer> colors = MaterialPalettes.getColorsByName(null, false);
+            List<Integer> colors = MaterialPalettes.INSTANCE.getColorsByName(null, false);
             // If we made it here, fail
             fail();
         } catch(IllegalArgumentException iae) {
@@ -165,9 +165,9 @@ public class TestMaterialPalettes extends AndroidTestCase{
         // For each level in the `NON_ACCENT_COLOR_LEVELS` we should return a list of colors
         // for each material design color palette. Verify count against size of color names.
         try {
-            for(String colorLevel : MaterialPalettes.NON_ACCENT_COLOR_LEVELS) {
-                List<Integer> colors = MaterialPalettes.getColorsByLevel(colorLevel);
-                assertEquals(MaterialPalettes.ALL_COLOR_NAMES.length, colors.size());
+            for(String colorLevel : MaterialPalettes.INSTANCE.getNON_ACCENT_COLOR_LEVELS()) {
+                List<Integer> colors = MaterialPalettes.INSTANCE.getColorsByLevel(colorLevel);
+                assertEquals(MaterialPalettes.INSTANCE.getALL_COLOR_NAMES().length, colors.size());
             }
         } catch(IllegalAccessException iae) {
             fail();
@@ -176,9 +176,9 @@ public class TestMaterialPalettes extends AndroidTestCase{
         // For each level in the `ACCENT_COLOR_LEVELS` we should return a list of colors
         // for each material design color with accents. Verify count against size of accent color list.
         try {
-            for(String colorLevel : MaterialPalettes.ACCENT_COLOR_LEVELS) {
-                List<Integer> colors = MaterialPalettes.getColorsByLevel(colorLevel);
-                assertEquals(MaterialPalettes.COLORS_WITH_ACCENT_NAMES.length, colors.size());
+            for(String colorLevel : MaterialPalettes.INSTANCE.getACCENT_COLOR_LEVELS()) {
+                List<Integer> colors = MaterialPalettes.INSTANCE.getColorsByLevel(colorLevel);
+                assertEquals(MaterialPalettes.INSTANCE.getCOLORS_WITH_ACCENT_NAMES().length, colors.size());
             }
         } catch(IllegalAccessException iae) {
             fail();
@@ -186,7 +186,7 @@ public class TestMaterialPalettes extends AndroidTestCase{
 
         // Test that we throw an IllegalArgumentException if an invalid string is passed
         try {
-            List<Integer> colors = MaterialPalettes.getColorsByName("");
+            List<Integer> colors = MaterialPalettes.INSTANCE.getColorsByName("");
             // If we made it here, fail
             fail();
         } catch(IllegalArgumentException iae) {
@@ -197,7 +197,7 @@ public class TestMaterialPalettes extends AndroidTestCase{
 
         // Test that we throw an IllegalArgumentException if null is passed
         try {
-            List<Integer> colors = MaterialPalettes.getColorsByLevel(null);
+            List<Integer> colors = MaterialPalettes.INSTANCE.getColorsByLevel(null);
             // If we made it here, fail
             fail();
         } catch(IllegalArgumentException iae) {
@@ -212,10 +212,10 @@ public class TestMaterialPalettes extends AndroidTestCase{
      */
     public void testGetAllColors() {
         // The expected count is one for each accent_colors and all color levels, and each non_accent_color and non_accent_color_levels
-        int expectedCount = (MaterialPalettes.ALL_COLOR_NAMES.length * MaterialPalettes.NON_ACCENT_COLOR_LEVELS.length) + (MaterialPalettes.COLORS_WITH_ACCENT_NAMES.length * MaterialPalettes.ACCENT_COLOR_LEVELS.length);
+        int expectedCount = (MaterialPalettes.INSTANCE.getALL_COLOR_NAMES().length * MaterialPalettes.INSTANCE.getNON_ACCENT_COLOR_LEVELS().length) + (MaterialPalettes.INSTANCE.getCOLORS_WITH_ACCENT_NAMES().length * MaterialPalettes.INSTANCE.getACCENT_COLOR_LEVELS().length);
 
         try {
-            List<Integer> colorList = MaterialPalettes.getAllColors();
+            List<Integer> colorList = MaterialPalettes.INSTANCE.getAllColors();
             assertEquals(expectedCount, colorList.size());
         } catch(IllegalAccessException iae) {
             fail();
@@ -228,12 +228,12 @@ public class TestMaterialPalettes extends AndroidTestCase{
     public void testGetRandomColor() {
         try {
             ArrayList<Integer> valueCheck = new ArrayList<>();
-            int colorSize = MaterialPalettes.getAllColors().size();
+            int colorSize = MaterialPalettes.INSTANCE.getAllColors().size();
 
             Log.d("RandomTest","Color size:" + colorSize);
 
             for (int i = 0; i < colorSize; i++) {
-                Integer color = MaterialPalettes.getRandomColorNonRepeating();
+                Integer color = MaterialPalettes.INSTANCE.getRandomColorNonRepeating();
                 if (valueCheck.contains(color)) {
                     fail();
                 }
@@ -254,12 +254,12 @@ public class TestMaterialPalettes extends AndroidTestCase{
         List<Integer> nonAccentColorList = new ArrayList<>();
 
         try {
-            for(String name : MaterialPalettes.ALL_COLOR_NAMES) {
-                nonAccentColorList.addAll(MaterialPalettes.getColorsByName(name, false));
+            for(String name : MaterialPalettes.INSTANCE.getALL_COLOR_NAMES()) {
+                nonAccentColorList.addAll(MaterialPalettes.INSTANCE.getColorsByName(name, false));
             }
 
             // Ensure it exists
-            int randomColor = MaterialPalettes.getRandomNonAccentColor();
+            int randomColor = MaterialPalettes.INSTANCE.getRandomNonAccentColor();
             assertTrue(nonAccentColorList.contains(randomColor));
         } catch(IllegalAccessException iae) {
             fail();
@@ -271,9 +271,9 @@ public class TestMaterialPalettes extends AndroidTestCase{
      */
     public void testGetAccentColorsByName() {
         try {
-            for(String name : MaterialPalettes.COLORS_WITH_ACCENT_NAMES) {
-                List<Integer> colorList = MaterialPalettes.getAccentColorsByName(name);
-                assertEquals(MaterialPalettes.ACCENT_COLOR_LEVELS.length, colorList.size());
+            for(String name : MaterialPalettes.INSTANCE.getCOLORS_WITH_ACCENT_NAMES()) {
+                List<Integer> colorList = MaterialPalettes.INSTANCE.getAccentColorsByName(name);
+                assertEquals(MaterialPalettes.INSTANCE.getACCENT_COLOR_LEVELS().length, colorList.size());
             }
         } catch(IllegalAccessException iae) {
             fail();
@@ -288,11 +288,11 @@ public class TestMaterialPalettes extends AndroidTestCase{
         List<Integer> accentColorList = new ArrayList<>();
 
         try {
-            for(String name : MaterialPalettes.COLORS_WITH_ACCENT_NAMES) {
-                accentColorList.addAll(MaterialPalettes.getAccentColorsByName(name));
+            for(String name : MaterialPalettes.INSTANCE.getCOLORS_WITH_ACCENT_NAMES()) {
+                accentColorList.addAll(MaterialPalettes.INSTANCE.getAccentColorsByName(name));
             }
             // Get random color, make sure it's in our accent list
-            Integer randomColor = MaterialPalettes.getRandomAccentColor();
+            Integer randomColor = MaterialPalettes.INSTANCE.getRandomAccentColor();
             assertTrue(accentColorList.contains(randomColor));
         } catch(IllegalAccessException iae) {
             fail();
@@ -305,9 +305,9 @@ public class TestMaterialPalettes extends AndroidTestCase{
     public void testGetRandomColorByLevel() {
         try {
             // Test this for each level
-            for(String level : MaterialPalettes.NON_ACCENT_COLOR_LEVELS) {
-                List<Integer> colorList = MaterialPalettes.getColorsByLevel(level);
-                Integer randomColor = MaterialPalettes.getRandomColorByLevel(level);
+            for(String level : MaterialPalettes.INSTANCE.getNON_ACCENT_COLOR_LEVELS()) {
+                List<Integer> colorList = MaterialPalettes.INSTANCE.getColorsByLevel(level);
+                Integer randomColor = MaterialPalettes.INSTANCE.getRandomColorByLevel(level);
                 assertTrue(colorList.contains(randomColor));
             }
         } catch(IllegalAccessException iae) {
@@ -321,9 +321,9 @@ public class TestMaterialPalettes extends AndroidTestCase{
     public void testGetRandomColorByName() {
         try {
             // Do this for every color
-            for(String name : MaterialPalettes.ALL_COLOR_NAMES) {
-                List<Integer> colorList = MaterialPalettes.getColorsByName(name);
-                Integer randomColor = MaterialPalettes.getRandomColorByName(name);
+            for(String name : MaterialPalettes.INSTANCE.getALL_COLOR_NAMES()) {
+                List<Integer> colorList = MaterialPalettes.INSTANCE.getColorsByName(name);
+                Integer randomColor = MaterialPalettes.INSTANCE.getRandomColorByName(name);
                 assertTrue(colorList.contains(randomColor));
             }
         } catch(IllegalAccessException iae) {
@@ -337,8 +337,8 @@ public class TestMaterialPalettes extends AndroidTestCase{
     public void testGetRandomColorWithLimits() {
         try {
             // Get a random red color, make sure it's in the list
-            List<Integer> redColors = MaterialPalettes.getColorsByName(MaterialPalettes.RED);
-            Integer randomColor = MaterialPalettes.getRandomColorWithLimits(new String[] { MaterialPalettes.RED } );
+            List<Integer> redColors = MaterialPalettes.INSTANCE.getColorsByName(MaterialPalettes.INSTANCE.getRED());
+            Integer randomColor = MaterialPalettes.INSTANCE.getRandomColorWithLimits(new String[] {MaterialPalettes.INSTANCE.getRED()} );
             assertTrue(redColors.contains(randomColor));
         } catch(IllegalAccessException iae) {
             fail();
